9b23989c576d663927de94aa1d69d3b6ca8e6319
AnatolyGulamov <36788487+AnatolyGulamov@users.noreply.github.com>
2018-02-28 21:06:14 +0700

    Square added

---
+++ Square/Square/SquareClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
+
+namespace Square
+{
+    public partial class SquareClass : Figure
+    {
+        public void Drawing(WrapPanel wrappanel)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 200;
+            rectangle.Height = 200;
+            rectangle.Fill = Brushes.Green;
+            rectangle.Stroke = Brushes.Black;
+            rectangle.StrokeThickness = 2;
+            wrappanel.Children.Add(rectangle);
+        }
+    }
+}
